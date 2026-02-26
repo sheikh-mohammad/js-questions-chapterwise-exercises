ddc74c0035956e1e46f44a5781d2e5e48596041d
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2026-02-27 04:20:10 +0500

    Implement question-04: Display order details using variables

---
+++ chapter-03-variables-for-numbers/question-04/app.js
@@ -0,0 +1,5 @@
+var visitorName = "Jhone Doe";
+var productTitle = "T-shirt";
+var productQuantity = 5;
+
+document.writeln(`<b>${visitorName}</b> ordered ${productQuantity} <b>${productTitle}</b>(s) on XYZ Clothing store`);

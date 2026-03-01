b6d2b56d9d5575dea449d788baf146e4b2bb076b
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2026-03-01 07:33:05 +0500

    Add question-07: Array operations

---
+++ chapter-14-16-arrays/question-07/app.js
@@ -0,0 +1,12 @@
+var eduQualfs;
+eduQualfs = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M. Phil.", "PhD"];
+
+document.writeln("Qualifications: <br /> <br />");
+document.writeln(`1) ${eduQualfs[0]} <br />`);
+document.writeln(`2) ${eduQualfs[1]} <br />`);
+document.writeln(`3) ${eduQualfs[2]} <br />`);
+document.writeln(`4) ${eduQualfs[3]} <br />`);
+document.writeln(`5) ${eduQualfs[4]} <br />`);
+document.writeln(`6) ${eduQualfs[5]} <br />`);
+document.writeln(`7) ${eduQualfs[6]} <br />`);
+document.writeln(`8) ${eduQualfs[7]} <br />`);

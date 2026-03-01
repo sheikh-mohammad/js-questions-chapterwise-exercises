49137b7b4f3df2d8c6f432f9c5104ea1e822e021
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2026-03-01 13:41:11 +0500

    Add question-11: Array slice method

---
+++ chapter-14-16-arrays/question-11/app.js
@@ -0,0 +1,8 @@
+var cities = ["Karachi", "Lahore", "Islamabad", "Quetta", "Peshawar"];
+
+var selectedCities = cities.slice(2, 4)
+
+document.writeln("Cities list: <br />");
+document.writeln(`${cities} <br /> <br />`);
+document.writeln("Selected cities list: <br />");
+document.writeln(selectedCities);

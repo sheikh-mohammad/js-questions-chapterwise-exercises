5acba2452a1a863a47f71999d7eddd73ae065bca
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2026-03-01 07:18:20 +0500

    Add question-03: Variable naming conventions

---
+++ chapter-04-variable-names-legal-and-illegal/question-03/app.js
@@ -0,0 +1,5 @@
+document.writeln(`Variable names can only contain , ${"numbers"}, ${"$"}, and ${"_"}. For example $my_1stVariable <br />`);
+document.writeln(`Variables must begin with a ${"letter"}, ${"$"} or
+${"_"}. For example : $name, _name or name <br />`);
+document.writeln(`Variable names are case ${"sensitive"} <br />`);
+document.writeln(`Variable names should not be JS ${"keywords"} <br />`);

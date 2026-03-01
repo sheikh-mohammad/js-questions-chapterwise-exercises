47b0ca419faed433744172c4d1680ad5f7419612
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2026-03-01 07:09:15 +0500

    Add question-02: Legal and illegal variable examples

---
+++ chapter-04-variable-names-legal-and-illegal/question-02/app.js
@@ -0,0 +1,15 @@
+// Legal Variables
+
+var legalVariable;
+var legalVariable1;
+var legal_variable;
+var $legalvariable;
+var $legal_Variable5;
+
+// Illegal Variables
+
+var 1illegalVariable;
+var illegal-variable;
+var illegal variable;
+var class;
+var illegal.variable;

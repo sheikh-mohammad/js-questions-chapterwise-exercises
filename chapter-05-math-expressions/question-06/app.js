ecfda475fb8f5040d666c4cff1d9390fa3633958
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2026-03-02 15:48:14 +0500

    Add question-06: Math expressions

--- chapter-05-math-expressions/question-06/app.js
+++ chapter-05-math-expressions/question-06/app.js
@@ -1,1 +1,5 @@
-// app.js for question-06 
+var celsiusTemp = 25;
+var fahrenTemp = 70;
+
+document.writeln(`${celsiusTemp}<sup>0</sup>C is ${(celsiusTemp * 9 / 5) + 32}<sup>o</sup>F <br />`);
+document.writeln(`${fahrenTemp}<sup>0</sup>F is ${(fahrenTemp - 32) * 5 / 9}<sup>o</sup>C <br />`);

5b4599900677562d9c054b76c61933cfecefff00
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2026-03-02 15:48:51 +0500

    Add question-13: Math expressions

--- chapter-05-math-expressions/question-13/app.js
+++ chapter-05-math-expressions/question-13/app.js
@@ -1,1 +1,12 @@
-// app.js for question-13 
+var favorSnack = "chocolate chip";
+var currentAge = 15;
+var maxAge = 65;
+var perDayAmount = 3;
+
+var restLifeEatings = (maxAge - currentAge) * 3
+
+document.writeln(`Favourite Snack: ${favorSnack} <br />`);
+document.writeln(`Current age: ${currentAge} <br />`);
+document.writeln(`Estimated Maximum Age: ${maxAge} <br />`);
+document.writeln(`Amount of snacks per day: ${perDayAmount} <br />`);
+document.writeln(`You will need ${restLifeEatings} to last you until the ripe old age of ${maxAge}`);

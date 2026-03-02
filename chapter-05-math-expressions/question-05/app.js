527152be72162ceeef6564efa639633e59dd9ee7
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2026-03-02 15:48:07 +0500

    Add question-05: Math expressions

--- chapter-05-math-expressions/question-05/app.js
+++ chapter-05-math-expressions/question-05/app.js
@@ -1,1 +1,12 @@
-// app.js for question-05 
+var multiTable = 4;
+
+document.writeln(`${multiTable}x1=${multiTable * 1} <br />`);
+document.writeln(`${multiTable}x2=${multiTable * 2} <br />`);
+document.writeln(`${multiTable}x3=${multiTable * 3} <br />`);
+document.writeln(`${multiTable}x4=${multiTable * 4} <br />`);
+document.writeln(`${multiTable}x5=${multiTable * 5} <br />`);
+document.writeln(`${multiTable}x6=${multiTable * 6} <br />`);
+document.writeln(`${multiTable}x7=${multiTable * 7} <br />`);
+document.writeln(`${multiTable}x8=${multiTable * 8} <br />`);
+document.writeln(`${multiTable}x9=${multiTable * 9} <br />`);
+document.writeln(`${multiTable}x10=${multiTable * 10} <br />`);

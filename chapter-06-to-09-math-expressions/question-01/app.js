b4c4bd7cdb9cf75dc69acfaf5757c42514871748
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2026-03-06 05:57:48 +0500

    Add increment/decrement operators demo to chapter-06-to-09-math-expressions/question-01

---
+++ chapter-06-to-09-math-expressions/question-01/app.js
@@ -0,0 +1,24 @@
+var a = 10;
+
+document.writeln("Result: <br />");
+document.writeln(`The value of a is: ${a} <br />`);
+
+document.writeln("............................................ <br /> <br />");
+
+document.writeln(`The value of ++a is: ${++a} <br />`);
+document.writeln(`Now the value of a is: ${a} <br />`);
+
+document.writeln("<br /> <br />");
+
+document.writeln(`The value of a++ is: ${++a} <br />`);
+document.writeln(`Now the value of a is: ${a} <br />`);
+
+document.writeln("<br /> <br />");
+
+document.writeln(`The value of --a is: ${--a} <br />`);
+document.writeln(`Now the value of a is: ${a} <br />`);
+
+document.writeln("<br /> <br />");
+
+document.writeln(`The value of a-- is: ${a--} <br />`);
+document.writeln(`Now the value of a is: ${a} <br />`);
